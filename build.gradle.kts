33fcb2d369360731634fe321f0be3ccc9400c05b
Nicolas Boutet <amd3002@gmail.com>
2026-09-19 11:26:28 +0200

    chore: update jackson-databind to 2.18.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,7 @@ val canaryImplementation: Configuration by configurations.getting {
 }
 
 dependencies {
-    api("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    api("com.fasterxml.jackson.core:jackson-databind:2.18.4")
 
     testImplementation(platform("org.junit:junit-bom:5.11.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
